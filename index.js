5760536bf2765e5726ccf1b15efb57ec8df7f6d7
Pawan Rijal <pawan.rijal@texasintl.edu.np>
2022-10-11 16:42:13 +0545

    added clock text

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 setInterval(() => {
-    d = new Date()
-    htime = d.getHours();
-    mtime = d.getMinutes();
-    stime = d.getSeconds();
+  const d = new Date();
+  const htime = d.getHours();
+  const mtime = d.getMinutes();
+  const stime = d.getSeconds();
 
-    hrotation = 30 * htime + mtime/2;
-    mrotation = 6 * mtime;
-    srotation = 6 * stime;
+  const hrotation = 30 * htime + mtime / 2;
+  const mrotation = 6 * mtime;
+  const srotation = 6 * stime;
 
-    document.getElementById("hour").style.transform = "rotate("+ hrotation +"deg)";
-    document.getElementById("minutes").style.transform = "rotate("+ mrotation +"deg)";
-    document.getElementById("seconds").style.transform = "rotate("+ srotation +"deg)";
-
-
-
+  document.getElementById("hour").style.transform =
+    "rotate(" + hrotation + "deg)";
+  document.getElementById("minutes").style.transform =
+    "rotate(" + mrotation + "deg)";
+  document.getElementById("seconds").style.transform =
+    "rotate(" + srotation + "deg)";
 }, 1000);
-
-
